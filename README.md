08da3f83681d7ef23fe1c3ede989865fd009b673
Pedro Sousa <pedro@pedletsinfo.com.br>
2019-06-15 23:27:11 -0300

    Envio de projeto pronto para GitLab

---
+++ README.md
@@ -0,0 +1,3 @@
+Exercicio solicitado em aula da Pagina de Tributo.
+
+Envio de projeto pronto para o GitLab
---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tributo a Mark Zuckenberg</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<header>
+		<div class="topo">
+			<div class="topoesq">
+				<a href="https://www.facebook.com/zuck" target="_blank">Mark Zuckenberg</a>
+			</div>
+				<div class="topodir">
+					<ul>
+						<li><a href="#glance" target="_top">Feitos</a></li>
+						<li><a href="#timeline">Linha do Tempo</a></li>
+						<li><a href="#more">Mais</a></li>
+						<li><a href="#sources">Fontes</a></li>
+					</ul>
+				</div>
+		</div>	
+	</header>
+	<content>
+		<div class="zuck">
+			<img src="assets/images/zuck.jfif"/>
+		</div>
+		<div class="zuck-credits">
+			<div class="zuck-text">Mark Zuckerberg. Fonte: <a href="https://beam.biz/content/7954/Facebooks_news_feed_will_soon_show_you_more_content_from_friends_and_family_and_less_from_publishers" target="_blank">beam.biz <img src="assets/images/setalink.png"/></a></div>
+		</div>
+		<div id="glance" class="zuck-atglance">
+			<span>Mark Zuckerberg os feitos</span><br/>
+			<p><b>Mark Elliot Zuckerberg</b> é um programador americano de computadores e empresário da Internet. Ele é co-fundador do Facebook e atualmente atua como presidente e diretor executivo. Seu patrimônio líquido é estimado em US $ 72,5 bilhões em 5 de março de 2018.</p>
+			<p>Zuckerberg lançou o Facebook a partir do seu quarto na Universidade de Harvard em <b>4 de fevereiro de 2004</b>, com seus colegas de Harvard, Eduardo Saverin, Andrew McCollum, Dustin Moskovitz e Chris Hughes. O grupo então introduziu o Facebook em outros campos universitários. O Facebook expandiu-se rapidamente, <b>alcançando um bilhão de usuários em 2012</b>. Durante esse período, Zuckerberg se envolveu em várias disputas legais trazidas por seus amigos e co-fundadores, que alegaram ter participação na empresa com base em seu envolvimento durante a fase de desenvolvimento.</p>
+		</div>
+		<div id="timeline" class="zuck-timeline">
+			<span>Linha do Tempo</span><br>
+			<p><b>14 de Maio de 1984 - </b> Nasceu em <a href="https://pt.wikipedia.org/wiki/White_Plains_(Nova_Iorque)" target="_blank">White Plains, Nova York</a>, o único filho de Edward que é dentista e de Kristen que é psiquiatra, tem três irmãs <a href="https://pt.wikipedia.org/wiki/Randi_Zuckerberg" target="_blank">Randi</a>, Donna e Arielle.</p>
+			<p><b>Setembro de 2002 - </b>Graduado pela <a href="https://www.exeter.edu/" target="_blank">Phillips Exeter Academy</a>, uma escola particular em <a href="https://pt.wikipedia.org/wiki/Nova_Hampshire" target="_blank">New Hampshire</a>; junta-se à <a href="https://pt.wikipedia.org/wiki/Universidade_Harvard" target="_blank">Universidade de Harvard</a>  no outono em um curso de <a href="https://pt.wikipedia.org/wiki/Psicologia" target="_blank">psicologia</a>  e <a href="https://pt.wikipedia.org/wiki/Ciência_da_computação" target="_blank">ciência da computação</a>.</p>
+			<p><b>28 de Outubro de 2003 - </b>lança o <a href="http://www.atenwood.com/facemash" target="_blank">Facemash</a>, um programa da web que permite que estudantes de Harvard comparem imagens de alunos do mesmo sexo e avaliem os mais atraentes, chegando a ser expulso da universidade mas entretanto todas as acusações foram retiradas.</p>
+			<p><b>Janeiro de 2004 - </b>Começa a escrever o codigo para um novo site conhecido como <i>"thefacebook"</i></p>
+			<p><b>4 de Fevereiro de 2004 - </b>Lançamento do <a href="https://www.facebook.com" target="_blank">Thefacebook.com</a></p>
+			<p><b>10 de Fevereiro de 2004 - </b>Os gêmeos Winklevoss entraram com uma ação judicial contra Zuckerberg alegando que ele roubou a ideia de uma nova rede social, a HarvardConnection (mais tarde, ConnectU).</p>
+			<p><b>Maio de 2004 - </b>Facebook atinge 200 mil usuários; contacta capitalistas de risco no Vale do Silício e recebe um investimento inicial de meio milhão de dólares - alguns dos quais são usados ​​para um pequeno escritório em cima de um restaurante chinês</p>
+			<p><b>Junho de 2004 - </b>Sai de Harvard no final do seu segundo ano; muda-se para <a href="https://pt.wikipedia.org/wiki/Palo_Alto" target="_blank">Palo Alto</a> naquele verão, onde ele aluga uma casa com uma piscina. Executivos do <a href="https://pt.wikipedia.org/wiki/Google" target="_blank">Google</a> foram visitá-los na tentativa de trabalhar em parceria ou comprar o Facebook.</p>
+			<p><b>Junho de 2004 - </b>Quatro meses após o lançamento do então <a href="https://www.facebook.com" target="_blank">Thefacebook.com</a>, Mark Zuckerberg recebeu proposta de um investidor não identificado de Nova York. Ele queria comprar o site por <b>US$ 10 milhões</b>, mas Zuckerberg não levou a proposta a sério.</p>
+			<p><b>2005 - </b>Zuckerberg conversava com o <a href="https://pt.wikipedia.org/wiki/The_Washington_Post" target="_blank">Washington Post</a> sobre investimentos quando recebeu uma proposta da <a href="https://pt.wikipedia.org/wiki/Viacom" target="_blank">Viacom</a>, que pagaria <b>US$ 75 milhões</b> – só Zuckerberg levaria <b>US$ 35 milhões</b>. Mas <a href="https://pt.wikipedia.org/wiki/Sean_Parker" target="_blank">Sean Parker</a>, então presidente da empresa, usou isso para negociar termos melhores com o Post, que não continuou a negociação.</p>
+			<p><b>2005 - </b>O pessoal do <a href="https://pt.wikipedia.org/wiki/Myspace" target="_blank">MySpace</a> também foi atrás do Facebook. Chris DeWokfe, CEO da companhia, falou com Sean Parker, Zuckerberg e Matt Cohler, quando Zuckerberg perguntou se ele compraria o Facebook por <b>US$ 75 milhões</b>, DeWolfe disse não – no fim do ano, a mesma pergunta, mas por <b>US$ 750 milhões</b>, e ele se negou novamente.</p>
+			<p><b>2006 - </b>Michel Wolf chefe da <a href="https://pt.wikipedia.org/wiki/MTV" target="_blank">MTV</a> (que é subsidiária da Viacom) foi atrás de Zuckenberg para a compra do Facebook e ouviu que o mesmo só seria vendido pelo minimo de <b>U$ 2 bilhões</b>. Algumas semanas depois a <a href="https://pt.wikipedia.org/wiki/Viacom" target="_blank">Viacom</a> colocou <b>US$ 1,5 bilhão</b> na mesa, dos quais <b>US$ 800 milhões</b> viriam adiantados no entanto Zuckenberg quis um adiantamento maior e a <a href="https://pt.wikipedia.org/wiki/Viacom" target="_blank">Viacom</a> desistiu da compra.</p>
+			<p><b>2006 - </b> O <a href="https://pt.wikipedia.org/wiki/Yahoo!" target="_blank">Yahoo!</a> ofereceu <b>US$ 1 bilhão</b> para comprar o Facebook. Apesar de vários investidores e executivos serem a favor do negócio, o site estava prestes a lançar o feed de notícias e Zuckerberg acreditava que a novidade poderia gerar um valor ainda maior. O Yahoo diminuiu a oferta para US$ 850 quando viu seus péssimos resultados financeiros do segundo trimestre do ano e o Facebook levou apenas 10 minutos para recusar. No fim de 2006, o <a href="https://pt.wikipedia.org/wiki/Yahoo!" target="_blank">Yahoo!</a> disse que pagaria até mais que o <b>US$ 1 bilhão</b> inicial, mas o Facebook abriu seus serviços a quem não era estudante de Harvard e viu um salto de pessoas registradas – de 20 mil a 50 mil. Era tarde demais para o Yahoo.</p>
+			<p><b>Setembro de 2007 - </b>Rejeita uma oferta de <b>US $ 15 bilhões</b> da <a href="https://pt.wikipedia.org/wiki/Microsoft" target="_blank">Microsoft</a>, dos quais <b>US $ 4 bilhões</b> seriam dele.</p>
+			<p><b>26 de Setembro de 2006 - </b>Abre o Facebook além das universidades para que todos acima de 13 anos com um endereço de e-mail possam participar.</p>
+			<p><b>Outubro de 2008 - </b>Estabelece a sua sede internacional em Dublin, Irlanda.</p>
+			<p><b>2008 - </b>Chega a um acordo com os irmãos Winklevoss em um processo no valor de US $ 65 milhões, embora ainda negue qualquer roubo de propriedade intelectual. Processo foi reaberto visto que os gemeos alegaram ter sido enganados sobre o valor real das ações do Facebook na epoca do acordo.</p>
+			<p><b>2009 - </b>A China bloqueia o acesso doméstico ao Facebook.</p>
+			<p><b>2010 - </b>Nomeado pessoa do ano da revista Time.</p>
+			<p><b>2010 - </b>Filme de Hollywood <a href="https://pt.wikipedia.org/wiki/A_Rede_Social" target="_blank">A Rede Social</a> é lançado documentando o tempo de Zuckerberg em Harvard e os primeiros dias do Facebook.</p>
+			<p><b>2011 - </b>Aparece no <a href="https://pt.wikipedia.org/wiki/Saturday_Night_Live" target="_blank">Saturday Night Live</a>, juntamente com o anfitrião <a href="https://pt.wikipedia.org/wiki/Jesse_Eisenberg">Jesse Eisenberg</a>, que o interpretou no filme.</p>
+			<p><b>12 de Abril de 2012 - </b>Aquisição do <a href="https://www.instagram.com/" target="_blank">Instagram</a> por <b>U$ 1 bilhão.</b></p>
+			<p><b>6 de Outubro de 2014 - </b>Aquisição do serviço móvel de mensagens instantâneas <a href="https://www.whatsapp.com/" target="_blank">WhatsApp</a> por aproximadamente <b>U$ 22 bilhões.</b> </p>
+		</div>			
+		<div id="more" class="zuck-more">
+			<span>Mais</span>
+		</div>
+		<div class="zuck-more-rectangle">
+			<p>O Facebook não foi originalmente criado para ser uma empresa. Foi construído para realizar uma missão social - tornar o mundo mais aberto e conectado.<br><br> 
+			<b>Mark Zuckerberg</b></p>		
+		</div>
+		<div id="sources" class="zuck-sources">
+			<span>Fontes</span>
+			<div class="zuck-sources-1">
+				<a href="https://pt.wikipedia.org/wiki/Mark_Zuckerberg" target="_blank">Wikipédia do Mark Zuckerberg <img src="assets/images/setalink2.png" /></a>
+			</div>
+			<div class="zuck-sources-2">
+				<a href="https://pt.wikipedia.org/wiki/Hist%C3%B3ria_do_Facebook" target="_blank">Wikipédia da História do Facebook <img src="assets/images/setalink2.png" /></a>
+			</div>
+			<div class="zuck-sources-2">
+				<a href="https://pt.wikipedia.org/wiki/Facebook" target="_blank">Wikipédia do Facebook <img src="assets/images/setalink2.png" /></a>
+			</div>
+			<div class="zuck-sources-2">
+				<a href="https://www.brainyquote.com/authors/mark_zuckerberg" target="_blank">Quotes Mark Zuckerberg <img src="assets/images/setalink2.png" /></a>
+			</div>
+			<div class="zuck-sources-2">
+				<a href="https://olhardigital.com.br/pro/noticia/10-empresas-que-tentaram-comprar-o-facebook/41408" target="_blank">Artigo do Olhar Digital tentativa de compra do Facebook <img src="assets/images/setalink2.png" /></a>
+			</div>
+		</div>				
+	</content>
+	<footer>
+		Copyright ₢2019 <a href="http://www.pedletsinfo.com.br" target="_blank">PedLets Info Service</a> - All Rights Reserved
+	</footer>
+</body>
+</html>
