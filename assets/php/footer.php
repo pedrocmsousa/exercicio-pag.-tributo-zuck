511fdd1e3939b37fb9cdd2e61c04694956e423a1
Pedro Sousa <sousacmpedro@gmail.com>
2020-05-03 21:04:28 -0300

    Terminado o footer, adicionadas todas as redes sociais do programador

---
+++ assets/php/footer.php
@@ -0,0 +1,36 @@
+<footer>
+	<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-bottom">
+		<!-- Apresentação da pagina -->
+		
+		<a class="navbar-brand" href="https://www.pedlets.com.br" target="_blank">PEDLETS DEVS</a>
+
+		<!-- Botão de collapse -->
+
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarFooter">	
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<!-- Menu do site -->
+
+		<div class="collapse navbar-collapse justify-content-center" id="navbarFooter">
+			<span class="navbar-text">Copyright ₢2020 Pedlets Desenvolvimento - All Rights Reserved</span>
+		</div>
+		<ul class="navbar-nav my-2 my-lg-0">
+				<li class="nav-item dropdown dropup">
+	      			<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+	      			Redes Sociais
+	     			</a>
+			    <div class="dropdown-menu bg-info">
+			        <a class="dropdown-item" href="https://www.facebook.com/pedrocmsousa" target="_blank"><img src="../assets/images/facebook.png"> <span>Facebook</span></a>
+			        <a class="dropdown-item" href="https://www.instagram.com/pedrocmsousa" target="_blank"><img src="../assets/images/instagram.png"> <span>Instagram</span></a>
+			        <a class="dropdown-item" href="https://wa.me/553175550661" target="_blank"><img src="../assets/images/whatsapp.png"> <span>WhatsApp</span></a>
+			        <div class="dropdown-divider"></div>
+			         <a class="dropdown-item" href="https://www.linkedin.com/in/pedrocmsousa" target="_blank"><img src="../assets/images/linkedin.png"> <span>Linkedin</span></a>
+			        <a class="dropdown-item" href=" https://twitter.com/pedrocmsousa" target="_blank"><img src="../assets/images/twitter.png"> <span>Twitter</span></a>
+			        <a class="dropdown-item" href="https://t.me/pedrocmsousa" target="_blank"><img src="../assets/images/telegram.png"> <span>Telegram</span></a>
+			        <div class="dropdown-divider"></div>
+			    </div>
+   			</li>
+		</ul>
+	</nav>
+</footer>
