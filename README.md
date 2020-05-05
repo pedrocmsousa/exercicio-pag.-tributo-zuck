ba348a40f9da3a4e93a6cbc0428f58c3e69e7799
Pedro Sousa <sousacmpedro@gmail.com>
2020-05-04 21:50:45 -0300

    Primeiro commit, inicio do projeto, criação de todos os arquivos, inicio do README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# 04/04/2020 21:48 
+
+- Inicio do projeto de links da Letícia
+- Montada a estrutura com o Bootstrap e arquivos style.css e script.js.
+- Todos os arquivos já carregados no index.php
+- 
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
+    <title>Links Letícia</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/images/favicon.ico" />
+</head>
+<body>
+    
+
+
+
+
+<!-- Carregamento de JS -->
+
+    <script type="text/javascript" src="assets/js/jquery-3.4.1.slim.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+</body>
+</html>
