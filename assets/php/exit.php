182deda55b91ad9ed20a8876d48d545e3e3f3df9
Pedro Sousa <sousacmpedro@gmail.com>
2020-05-14 01:01:51 -0300

    apagar tudo

--- assets/php/exit.php
+++
@@ -1,7 +0,0 @@
-<?php  
-session_start();
-
-unset($_SESSION['mkt']);
-header("Location: https://links.pedlets.com.br/");
-exit;
-?>
--- assets/php/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-	<nav class="navbar navbar-expand-lg navbar-danger text-dark">
-		<div class="collapse navbar-collapse justify-content-center" id="navbarFooter">
-			<a class="text-reset btn btn-info" href="https://www.pedlets.com.br" target="_blank">Copyright ₢2020 Pedlets Desenvolvimento - All Rights Reserved</a>
-		</div>
-	</nav>
-</footer>
